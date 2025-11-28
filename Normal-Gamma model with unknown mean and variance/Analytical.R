8f420160bb72d6be40c6ecbd549825a1cbcc34a1
Nathan Higgins <146757578+nathanhiggins03@users.noreply.github.com>
2025-11-28 21:29:46 +0000

    Update Analytical.R
    Wall clock time

--- Normal-Gamma model with unknown mean and variance/Analytical.R
+++ Normal-Gamma model with unknown mean and variance/Analytical.R
@@ -1,3 +1,4 @@
+start=proc.time()
 #Normal-Gamma model with unknown mean and variance
 
 #Set seed for reproducibility
@@ -23,5 +24,8 @@ beta1<- beta0 + 1/2 * (n-1) * var(x) + (k0 *n * (mean(x) - m0)^2)/(2 * k1)
 true_le<- log(gamma(alpha1)) -log(gamma(alpha0)) + alpha0* log(beta0) - alpha1 * log(beta1) + 0.5*(log(k0)-log(k1)) - n/2 * log(2*pi)
 true_le
 
+end=proc.time()
 
+timer<- end-start
+timer[3]
 
