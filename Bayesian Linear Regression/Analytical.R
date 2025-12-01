d6aa64e0f4646d776b6a01425f99ce87139262f6
Nathan Higgins <146757578+nathanhiggins03@users.noreply.github.com>
2025-12-01 20:10:51 +0000

    Update Analytical.R

--- Bayesian Linear Regression/Analytical.R
+++ Bayesian Linear Regression/Analytical.R
@@ -1,1 +1,42 @@
+#Bayesian Linear Regression with unknown beta and precision
+set.seed(123)
+#Data
+#y=2+3x+ϵ,ϵ∼N(0,0.5^2)
+
+N <- 30
+x <- runif(N, 0, 5)
+X <- cbind(1, x)   # include intercept
+beta_true <- c(2, 3)
+sigma_true <- 0.5
+y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
+
+#Data we have is
+X
+y
+
+#Prior inputs
+d <- ncol(X)
+m0 <- rep(1, d)
+Lambda0 <- diag(5, d)   # vague prior
+alpha0 <- 3
+beta0  <- 2 * var(y)
+
+
+#Posterior distribution inputs
+LambdaN <- Lambda0 + t(X) %*% X
+mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
+alphaN <- alpha0 + N / 2
+betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
+
+
+#Analytical log evidence
+
+term1 <- as.numeric(0.5 * (determinant(Lambda0, log = TRUE)$modulus - determinant(LambdaN, log = TRUE)$modulus))
+term2 <- alpha0 * log(beta0) - alphaN * log(betaN)
+term3 <- log(gamma(alphaN)) - log(gamma(alpha0))
+term4 <- -(N/2) * log(2*pi)
+true_le<- term1 + term2 + term3 + term4
+
+
+
 
