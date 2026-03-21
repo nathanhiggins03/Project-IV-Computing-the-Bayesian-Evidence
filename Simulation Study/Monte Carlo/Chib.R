0743a4bc477c3bd630f41afe1c6faa4209c24541
Nathan Higgins <146757578+nathanhiggins03@users.noreply.github.com>
2026-03-21 19:28:17 +0000

    Update Chib.R

--- Simulation Study/Monte Carlo/Chib.R
+++ Simulation Study/Monte Carlo/Chib.R
@@ -1,12 +1,46 @@
-#Need to change to incorporate more efficient code for HME
 library(ggplot2)
 
 Sim <- 30
-MC_values <- c(10,100, 10000,100000)   # <-- choose MC sizes
+MC_values <- c(50,100,500, 1000)   # <-- choose MC sizes
 
 df_all <- data.frame()
+#Data
+set.seed(123)
+#y=2+3x+ϵ,ϵ∼N(0,0.5^2)
+N <- 30
+x <- runif(N, 0, 5)
+X <- cbind(1, x)   # include intercept
+beta_true <- c(2, 3)
+sigma_true <- 0.5
+y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
+
+#Data we have is
+X
+y
+
+#Prior inputs
+d <- ncol(X)
+m0 <- rep(1, d)
+Lambda0 <- diag(5, d)   # vague prior
+alpha0 <- 3
+beta0  <- 36
 
 
+#Posterior distribution inputs
+LambdaN <- Lambda0 + t(X) %*% X
+mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
+alphaN <- alpha0 + N / 2
+betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
+
+#True value
+#Analytical log evidence
+
+term1 <- as.numeric(0.5 * (determinant(Lambda0, log = TRUE)$modulus - determinant(LambdaN, log = TRUE)$modulus))
+term2 <- alpha0 * log(beta0) - alphaN * log(betaN)
+term3 <- log(gamma(alphaN)) - log(gamma(alpha0))
+term4 <- -(N/2) * log(2*pi)
+true_le<- term1 + term2 + term3 + term4
+true_le
 for (MC_sample in MC_values) {
   
   time_simulation <- rep(0, Sim)
@@ -20,33 +54,6 @@ for (MC_sample in MC_values) {
     
     #Set seed for reproducibility
     set.seed(123 + k)
-    #Data
-    #y=2+3x+ϵ,ϵ∼N(0,0.5^2)
-    
-    N <- 30
-    x <- runif(N, 0, 5)
-    X <- cbind(1, x)   # include intercept
-    beta_true <- c(2, 3)
-    sigma_true <- 0.5
-    y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
-    
-    #Data we have is
-    X
-    y
-    
-    #Prior inputs
-    d <- ncol(X)
-    m0 <- rep(1, d)
-    Lambda0 <- diag(5, d)   # vague prior
-    alpha0 <- 3
-    beta0  <- 36
-    
-    
-    #Posterior distribution inputs
-    LambdaN <- Lambda0 + t(X) %*% X
-    mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
-    alphaN <- alpha0 + N / 2
-    betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
     
     
     #Chibs Method
@@ -79,7 +86,7 @@ for (MC_sample in MC_values) {
       return(mat)
     }
     
-    set.seed(3421)
+
     out1=gibbs(N=MC_sample,d=d, X=X,y=y, m0=m0, Lambda0=Lambda0, alpha0=alpha0, beta0=beta0)
     #Column mean
     colMeans(out1)
@@ -165,6 +172,8 @@ for (MC_sample in MC_values) {
   )
   
 }
+
+
 
 
 #Plotting evidence violin plots against MC sample size
