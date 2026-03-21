86a6addb24ec6c9cb775a1b2518839a2e75140db
Nathan Higgins <146757578+nathanhiggins03@users.noreply.github.com>
2026-03-21 18:25:19 +0000

    Update Prior method.R

--- Simulation Study/Monte Carlo/Prior method.R
+++ Simulation Study/Monte Carlo/Prior method.R
@@ -16,6 +16,46 @@ prior_model <- stan_model(
   file = "Prior Bayesian Linear Regression.stan"
 )
 
+#True value
+#Data
+#y=2+3x+ϵ,ϵ∼N(0,0.5^2)
+set.seed(123)
+N <- 30
+x <- runif(N, 0, 5)
+X <- cbind(1, x)   # include intercept
+beta_true <- c(2, 3)
+sigma_true <- 0.5
+y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
+
+#Data we have is
+X
+y
+
+#Prior inputs
+d <- ncol(X)
+m0 <- rep(1, d)
+Lambda0 <- diag(5, d)   # vague prior
+alpha0 <- 3
+beta0  <- 36
+
+
+#Posterior distribution inputs
+LambdaN <- Lambda0 + t(X) %*% X
+mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
+alphaN <- alpha0 + N / 2
+betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
+
+
+#Analytical log evidence
+
+term1 <- as.numeric(0.5 * (determinant(Lambda0, log = TRUE)$modulus - determinant(LambdaN, log = TRUE)$modulus))
+term2 <- alpha0 * log(beta0) - alphaN * log(betaN)
+term3 <- log(gamma(alphaN)) - log(gamma(alpha0))
+term4 <- -(N/2) * log(2*pi)
+true_le<- term1 + term2 + term3 + term4
+true_le
+
+
 for (MC_sample in MC_values) {
   
   time_simulation <- rep(0, Sim)
@@ -26,34 +66,6 @@ for (MC_sample in MC_values) {
     start=proc.time()
     #Set seed for reproducibility
     set.seed(123+k)
-    #Data
-    #y=2+3x+ϵ,ϵ∼N(0,0.5^2)
-    
-    N <- 30
-    x <- runif(N, 0, 5)
-    X <- cbind(1, x)   # include intercept
-    beta_true <- c(2, 3)
-    sigma_true <- 0.5
-    y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
-    
-    #Data we have is
-    X
-    y
-    
-    #Prior inputs
-    d <- ncol(X)
-    m0 <- rep(1, d)
-    Lambda0 <- diag(5, d)   # vague prior
-    alpha0 <- 3
-    beta0  <- 36
-    
-    
-    #Posterior distribution inputs
-    LambdaN <- Lambda0 + t(X) %*% X
-    mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
-    alphaN <- alpha0 + N / 2
-    betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
-    
     
     
     #Mean of likelihood using prior samples:
@@ -116,44 +128,7 @@ for (MC_sample in MC_values) {
   
 }
 
-#True value
-#Data
-#y=2+3x+ϵ,ϵ∼N(0,0.5^2)
-set.seed(123)
-N <- 30
-x <- runif(N, 0, 5)
-X <- cbind(1, x)   # include intercept
-beta_true <- c(2, 3)
-sigma_true <- 0.5
-y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
 
-#Data we have is
-X
-y
-
-#Prior inputs
-d <- ncol(X)
-m0 <- rep(1, d)
-Lambda0 <- diag(5, d)   # vague prior
-alpha0 <- 3
-beta0  <- 36
-
-
-#Posterior distribution inputs
-LambdaN <- Lambda0 + t(X) %*% X
-mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
-alphaN <- alpha0 + N / 2
-betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
-
-
-#Analytical log evidence
-
-term1 <- as.numeric(0.5 * (determinant(Lambda0, log = TRUE)$modulus - determinant(LambdaN, log = TRUE)$modulus))
-term2 <- alpha0 * log(beta0) - alphaN * log(betaN)
-term3 <- log(gamma(alphaN)) - log(gamma(alpha0))
-term4 <- -(N/2) * log(2*pi)
-true_le<- term1 + term2 + term3 + term4
-true_le
 
 
 #Plotting evidence violin plots against MC sample size
