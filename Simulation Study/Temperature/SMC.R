0482b123f27bb271e326d84aaaad2f89329b4c0d
Nathan Higgins <146757578+nathanhiggins03@users.noreply.github.com>
2026-03-22 16:33:21 +0000

    Update SMC.R

--- Simulation Study/Temperature/SMC.R
+++ Simulation Study/Temperature/SMC.R
@@ -15,6 +15,44 @@ rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 prior_model <- stan_model("Prior Bayesian Linear Regression.stan")
 
+#Data
+set.seed(123)
+#y=2+3x+ϵ,ϵ∼N(0,0.5^2)
+N <- 30
+x <- runif(N, 0, 5)
+X <- cbind(1, x)   # include intercept
+beta_true <- c(2, 3)
+sigma_true <- 0.5
+y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
+
+#Data we have is
+X
+y
+
+#Prior inputs
+d <- ncol(X)
+m0 <- rep(1, d)
+Lambda0 <- diag(5, d)   # vague prior
+alpha0 <- 3
+beta0  <- 36
+
+
+#Posterior distribution inputs
+LambdaN <- Lambda0 + t(X) %*% X
+mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
+alphaN <- alpha0 + N / 2
+betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
+
+#True value
+#Analytical log evidence
+
+term1 <- as.numeric(0.5 * (determinant(Lambda0, log = TRUE)$modulus - determinant(LambdaN, log = TRUE)$modulus))
+term2 <- alpha0 * log(beta0) - alphaN * log(betaN)
+term3 <- log(gamma(alphaN)) - log(gamma(alpha0))
+term4 <- -(N/2) * log(2*pi)
+true_le<- term1 + term2 + term3 + term4
+true_le
+
 for (T_sample in T_values) {
   
   time_simulation <- rep(0, Sim)
@@ -28,36 +66,7 @@ for (T_sample in T_values) {
     
     #Set seed for reproducibility
     set.seed(123 + k)
-    #Data
-    #y=2+3x+ϵ,ϵ∼N(0,0.5^2)
-    
-    N <- 30
-    x <- runif(N, 0, 5)
-    X <- cbind(1, x)   # include intercept
-    beta_true <- c(2, 3)
-    sigma_true <- 0.5
-    y <- as.vector(X %*% beta_true + rnorm(N, 0, sigma_true))
-    
-    #Data we have is
-    X
-    y
-    
-    #Prior inputs
-    d <- ncol(X)
-    m0 <- rep(1, d)
-    Lambda0 <- diag(5, d)   # vague prior
-    alpha0 <- 3
-    beta0  <- 36
-    
-    
-    #Posterior distribution inputs
-    LambdaN <- Lambda0 + t(X) %*% X
-    mN <- solve(LambdaN, Lambda0 %*% m0 + t(X) %*% y)
-    alphaN <- alpha0 + N / 2
-    betaN <- beta0 + 0.5 * (t(y) %*% y + t(m0) %*% Lambda0 %*% m0 - t(mN) %*% LambdaN %*% mN)
-    
-    
-    
+  
     #SMC
     
     #Now create log power posterior function in R
@@ -79,7 +88,7 @@ for (T_sample in T_values) {
     T<- T_sample
     
     #N is number of (gibbs) samples for each tempered distribution
-    Nsim<-500
+    Nsim<-1000
     
     #Sample from joint prior
     
